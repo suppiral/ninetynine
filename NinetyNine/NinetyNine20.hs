b7de644238154b77922d43f48c84ec5de606b981
suppiral <suppiral@yahoo.com>
2014-02-23 19:01:05 +0200

    solved 18 to 20, first 20 problenas

--- NinetyNine/NinetyNine20.hs
+++ NinetyNine/NinetyNine20.hs
@@ -7,12 +7,16 @@ module NinetyNine20
 , repli
 , dropEvery
 , split
+, slice
+, rotate
+, removeAt
 ) where
 
 import NinetyNine10
 
 data EncodeItem a = Multiple Int a | Single a | None deriving (Show)
 
+-- 11
 encodeModified :: (Eq a) => [a] -> [EncodeItem a]
 encodeModified xs = map (encodeTupleToItem) $ encode xs
 
@@ -21,6 +25,7 @@ encodeTupleToItem (0, _) = None
 encodeTupleToItem (1, x) = Single x
 encodeTupleToItem (num, x) = Multiple num x
 
+-- 12
 decodeModified :: (Eq a) => [EncodeItem a] -> [a]
 decodeModified xs = concatMap decodeItem xs
 
@@ -34,6 +39,7 @@ replicate' num x
     | num > 0 = x:(replicate (num-1) x)
     | otherwise = []
 
+-- 13
 encodeDirect :: (Eq a) => [a] -> [EncodeItem a]
 encodeDirect [] = []
 encodeDirect (x:xs) = encodeDirect' 1 x xs
@@ -44,12 +50,15 @@ encodeDirect' num x (y:ys)
     | x == y = encodeDirect' (num+1) y ys
     | otherwise = encodeTupleToItem (num, x) : (encodeDirect' 1 y ys)
 
+-- 14
 dupli :: [a] -> [a]
 dupli xs = concatMap (replicate 2) xs
 
+-- 15
 repli :: [a] -> Int -> [a]
 repli xs n = concatMap (replicate n) xs
 
+-- 16
 dropEvery :: [a] -> Int -> [a]
 dropEvery xs n = dropEvery' xs n n
 
@@ -58,6 +67,7 @@ dropEvery' [] _ _ = []
 dropEvery' (x:xs) n 1 = dropEvery' xs n n
 dropEvery' (x:xs) n count = x:(dropEvery' xs n (count-1))
 
+-- 17
 split :: Int -> [a] -> ([a],[a])
 split n xs = rev $ split' n [] xs
     where rev (xs, ys) = (reverse xs, ys)
@@ -68,3 +78,22 @@ split' n xs [] = (xs, [])
 split' n xs ys@(z:zs)
     | n > 0 = split' (n-1) (z:xs) zs
     | otherwise = split' (length ys + n) xs ys
+
+-- 18
+slice :: Int -> Int -> [a] -> [a]
+slice i k list@(x:xs)
+    | i > 1 && k > i = slice (i-1) (k-1) xs
+    | i == 1 && k > i = fst $ split k list
+    | otherwise = []
+
+-- 19
+rotate :: Int -> [a] -> [a]
+rotate n xs = appendy . switchy $ split n xs
+    where switchy (a,b) = (b,a)
+          appendy (xs,ys) = xs ++ ys
+
+-- 20
+-- We can use Maybe to denote failure of removal due to non-existant member but this is out of the scope of the problem.
+removeAt :: Int -> [a] -> (a,[a])
+removeAt n xs = remAt $ split (n-1) xs
+    where remAt (xs,y:ys) = (y, xs ++ ys)
