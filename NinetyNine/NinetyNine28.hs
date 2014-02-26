93c910e9675ce684d7455873447c9691a64f704c
suppiral <suppiral@yahoo.com>
2014-02-26 21:13:47 +0200

    solved problenas 23 and 24

--- NinetyNine/NinetyNine28.hs
+++ NinetyNine/NinetyNine28.hs
@@ -1,9 +1,13 @@
 module NinetyNine28
 ( insertAt
 , range
+, rnd_select
+, diff_select
 ) where
 
 import NinetyNine20
+import System.Random (randomRIO)
+import Control.Monad (replicateM)
 
 insertAt :: a -> [a] -> Int -> [a]
 insertAt o xs n = appendo o $ split (n-1) xs
@@ -13,5 +17,13 @@ range :: Int -> Int -> [Int]
 range a b = [a..b]
 
 rangeAlt a b
-	| a <= b = a : (rangeAlt (a+1) b)
+    | a <= b = a : (rangeAlt (a+1) b)
     | a > b = []
+
+rnd_select :: [a] -> Int -> IO [a]
+rnd_select xs n = do
+    indexes <- replicateM n $ randomRIO (0, length xs-1)
+    return $ map (xs !!) indexes
+
+diff_select :: Int -> Int -> IO [Int]
+diff_select n r = rnd_select [1..r] n
