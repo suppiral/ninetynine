a70b5683a9bebda2f5bc486bcd41bbf2282adbae
suppi <soupiral@gmail.com>
2014-04-14 13:13:01 +0300

    31 - primes

---
+++ NinetyNine/Arithmetics.hs
@@ -0,0 +1,21 @@
+module Arithmetics
+( isPrime
+, primes
+--, gcd
+--, coprime
+) where
+
+mapWhile :: (a -> Bool) -> [a] -> Bool
+mapWhile f [] = False
+mapWhile f (x:xs)
+    | f x == True = True
+    | otherwise = mapWhile f xs
+
+isPrime :: Int -> Bool
+isPrime x
+    | x < 2 = False
+    | otherwise = not $ mapWhile (isModZero x) $ takeWhile (<=(ceiling . sqrt . fromIntegral) x) [2..]
+         where isModZero n y = n `mod` y == 0 && n /= 2
+
+primes :: [Int]
+primes = [n | n <- [2..], isPrime n]
