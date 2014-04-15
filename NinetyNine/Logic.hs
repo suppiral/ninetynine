b002f39af313e4b11d2e7e8e134d841759af736f
suppi <soupiral@gmail.com>
2014-04-15 22:32:38 +0300

    Logic tablen. up to 49 (without infixes)

--- NinetyNine/Logic.hs
+++ NinetyNine/Logic.hs
@@ -3,7 +3,9 @@ module Logic
 , table
 ) where
 
+import Control.Monad (replicateM)
 import Control.Applicative
+import Data.List (foldl', foldl1)
 
 not' :: Bool -> Bool
 not' True = False
@@ -33,3 +35,30 @@ equ' a = not . xor' a
 table :: (Bool -> Bool -> Bool) -> IO ()
 table f = mapM_ putStrLn $ g f <$> [False, True] <*> [False, True]
     where g f a b = show a ++ " " ++ show b ++ " " ++ show (f a b)
+
+-- 47
+infixl 6 `and'`
+infixl 4 `or'`
+
+
+-- 48
+
+and'',or'',nor'',nand'',xor'',impl'',equ'' :: [Bool] -> Bool
+
+and'' = foldl' and' True
+
+or'' = foldl' or' False
+
+nor'' = not . or''
+
+nand'' = not . and''
+
+xor'' = foldl' xor' False
+
+impl'' zs@(x:xs) = and'' $ zipWith impl' zs xs
+
+equ'' zs@(x:xs) = and'' $ zipWith equ' zs xs
+
+tablen :: Int -> ([Bool] -> Bool) -> IO ()
+tablen n f = mapM_ putStrLn $ map (\a -> listToStr a ++ " " ++ (show . f) a) $ replicateM n [False, True]
+    where listToStr = unwords . map show
